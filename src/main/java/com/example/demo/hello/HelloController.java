d399e89f65b0769092499958ccea4055573809a5
akiosako <vaticano2662@gmail.com>
2022-08-25 23:38:13 +0900

    現在の日付と時刻を表示する機能を追加

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,11 +1,17 @@
 package com.example.demo.hello;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HelloController {
+    @Autowired
+    private HelloService service;
     @GetMapping("/hello")
-    public String hello(){
+    public String hello(Model model){
+        String currentTime = service.getCurrentTime();
+        model.addAttribute("currentTime",currentTime);
         return "hello";
     }
-
 }
