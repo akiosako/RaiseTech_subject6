47d6cb8a1d6403bad53a2641f14c48dbaf452cfc
akiosako <vaticano2662@gmail.com>
2022-08-26 11:19:12 +0900

    hello.htmlからresponse.htmlへ画面遷移し現在の日付と時刻を表示するよう変更

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,4 +1,5 @@
 package com.example.demo.hello;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,12 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "/hello";
+    }
     @Autowired
     private HelloService service;
-    @GetMapping("/hello")
-    public String hello(Model model){
-        String currentTime = service.getCurrentTime();
-        model.addAttribute("currentTime",currentTime);
-        return "hello";
+    @GetMapping("/response")
+    public String helloResponse(Model model){
+       String currentTime = service.getCurrentTime();
+       model.addAttribute("currentTime",currentTime);
+        return "/response";
     }
 }
+
